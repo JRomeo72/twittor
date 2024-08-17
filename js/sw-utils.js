643b080b1d926d51e24e41c365b6a9f407940665
Romeo <spacework@Romeos-iMac.local>
2024-08-16 20:07:03 -0600

    Varios ajustes 2

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -2,11 +2,22 @@
 function updateDynamicCache ( dynamicCache, req, res ) {
 
     if( res.ok ) {
-        return caches.open( dynamicCache ).then( cache => {
-            cache.put( req, res.clone() )
-                .catch(err => console.log(err.message));
-            return res.clone()
-        } )
+
+        if(req.url.indexOf('chrome-extension') === -1) {
+            return caches.open( dynamicCache ).then( cache => {
+                cache.put( req, res.clone() )
+                    // .catch(err => {
+                    //     console.log("Req:", req);
+                    //     console.log("Res:", res.clone());
+                    //     console.log(err.message)
+                    // });
+                return res.clone()
+            } )
+        } else {
+            console.log("caching: ", req.url);
+            return res
+        }
+
     } else {
         return res
     }
--- sw.js
+++ sw.js
@@ -25,7 +25,7 @@ const APP_SHELL = [
 const APP_SHELL_INMUTABLE = [
     // 'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     // 'https://fonts.googleapis.com/css?family=Lato:400,300',
-    // 'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
     'css/animate.css',
     'js/libs/jquery.js'
 ];
@@ -64,9 +64,10 @@ self.addEventListener('fetch', e => {
         if( res ) {
             return res
         } else {
-            return fetch( e.request ).then( newRes => {
-                return updateDynamicCache( DYNAMIC_CACHE, e.request, newRes )
-            } )
+            return fetch( e.request )
+                .then( newRes => {
+                    return updateDynamicCache( DYNAMIC_CACHE, e.request, newRes )
+                })
         }
     })
 
