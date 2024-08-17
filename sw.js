7d8cfe0bc1be9694e6bd3e1aa80c34253e728d08
Romeo <spacework@Romeos-iMac.local>
2024-08-16 23:31:19 -0600

    Optimización para IOS

--- sw.js
+++ sw.js
@@ -1,23 +1,45 @@
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
-const APP_SHELL = [
 
-    // '/',
-    'index.html',
-    'css/style.css',
-    'img/favicon.ico',
-    'img/avatars/hulk.jpg',
-    'img/avatars/ironman.jpg',
-    'img/avatars/spiderman.jpg',
-    'img/avatars/thor.jpg',
-    'img/avatars/wolverine.jpg',
-    'js/app.js',
-    'js/sw-utils.js'
-];
+const APP_SHELL = [];
+
+if(url.includes('localhost')) {
+    APP_SHELL = [
+
+        '/',
+        'index.html',
+        'css/style.css',
+        'img/favicon.ico',
+        'img/avatars/hulk.jpg',
+        'img/avatars/ironman.jpg',
+        'img/avatars/spiderman.jpg',
+        'img/avatars/thor.jpg',
+        'img/avatars/wolverine.jpg',
+        'js/app.js',
+        'js/sw-utils.js'
+    ];
+} else {
+    APP_SHELL = [
+
+        '/twittor/',
+        'index.html',
+        'css/style.css',
+        'img/favicon.ico',
+        'img/avatars/hulk.jpg',
+        'img/avatars/ironman.jpg',
+        'img/avatars/spiderman.jpg',
+        'img/avatars/thor.jpg',
+        'img/avatars/wolverine.jpg',
+        'js/app.js',
+        'js/sw-utils.js'
+    ];
+}
+
+
 
 // fetch('https://fonts.googleapis.com/css?family=Quicksand:300,400')
 //     .then(res => res.text()).then(console.log)
@@ -48,6 +70,10 @@ self.addEventListener('activate', e => {
     let res = caches.keys().then( keys => {
         keys.map( key => {
             if( key !== STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key)
+            }
+
+            if( key !== INMUTABLE_CACHE && key.includes('inmutable')) {
                 return caches.delete(key)
             }
         } )
